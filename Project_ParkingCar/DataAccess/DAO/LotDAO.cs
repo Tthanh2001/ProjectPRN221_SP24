d2d705262b22d0dd57bce4f44d073c7c09afb26a
Tthanh2001 <77046685+Tthanh2001@users.noreply.github.com>
2024-02-25 18:49:31 +0700

    project

---
+++ Project_ParkingCar/DataAccess/DAO/LotDAO.cs
@@ -0,0 +1,87 @@
+﻿using DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.DAO
+{
+    public class LotDAO
+    {
+        private static LotDAO instance = null;
+        private static readonly object iLock = new object();
+        public LotDAO()
+        {
+        }
+
+        public static LotDAO Instance
+        {
+            get
+            {
+                lock (iLock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new LotDAO();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        public IEnumerable<Lot> GetAll()
+        {
+            List<Lot> lots;
+            try
+            {
+                var db = new ParkingDBWpfContext();
+                lots = db.Lots.Include(c => c.Invoices).Include(c => c.Type).ToList();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            return lots;
+        }
+
+        public Lot GetById(string id)
+        {
+            Lot lot = null;
+            try
+            {
+                var db = new ParkingDBWpfContext();
+                lot = db.Lots.Include(c => c.Type).Include(c => c.Invoices).SingleOrDefault(c => c.LotId.Equals(id));
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            return lot;
+        }
+
+        public void UpdateStatus(string id, bool status)
+        {
+            try
+            {
+                Lot _lot = GetById(id);
+                if (_lot != null)
+                {
+                    var db = new ParkingDBWpfContext();
+                    _lot.Status = status;
+                    db.Lots.Update(_lot);
+                    db.SaveChanges();
+                }
+                else
+                {
+                    throw new Exception("Lot does not exist!!!");
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
